36f7f67b05dfd1d26c83dd72c811de7309f20003
gcangini <233731286+gcangini@users.noreply.github.com>
2026-02-14 10:56:49 +0100

    Change logo image tag in header

--- app/Views/index.php
+++ app/Views/index.php
@@ -23,7 +23,7 @@
         </div>
         
         <div class="header-center">
-            <h1><img src="<?= base_url('img/favicon.svg') ?>" width="32px"> gWordle</h1>
+            <h1><img src="<?= base_url('img/favicon.svg') ?>" class="logo"> gWordle</h1>
         </div>
 
         <div class="header-right">
